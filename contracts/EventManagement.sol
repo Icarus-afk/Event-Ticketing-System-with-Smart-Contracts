9dbae1110b2f409195e1023cf8bf08ce2a36c204
Icarus-afk <ahmed.ehsan1258@gmail.com>
2024-03-16 22:27:13 +0600

    extending the smart contracts

--- contracts/EventManagement.sol
+++ contracts/EventManagement.sol
@@ -19,25 +19,61 @@ contract EventManagement {
     mapping(uint256 => mapping(address => uint256)) public ticketsSold;
 
     // Event creation event
-    event EventCreated(uint256 indexed eventId, string name, uint256 date, uint256 time, uint256 price, uint256 totalTickets);
+    event EventCreated(
+        uint256 indexed eventId,
+        string name,
+        uint256 date,
+        uint256 time,
+        uint256 price,
+        uint256 totalTickets
+    );
 
     // Modifier to check if the caller is the event organizer
     modifier onlyOrganizer(uint256 _eventId) {
-        require(msg.sender == events[_eventId].organizer, "Caller is not event organizer");
+        require(
+            msg.sender == events[_eventId].organizer,
+            "Caller is not event organizer"
+        );
         _;
     }
 
     // Function to create an event
-    function createEvent(string memory _name, uint256 _date, uint256 _time, uint256 _price, uint256 _totalTickets) public {
-        Event memory newEvent = Event(_name, _date, _time, _price, _totalTickets, msg.sender);
+    function createEvent(
+        string memory _name,
+        uint256 _date,
+        uint256 _time,
+        uint256 _price,
+        uint256 _totalTickets
+    ) public {
+        Event memory newEvent = Event(
+            _name,
+            _date,
+            _time,
+            _price,
+            _totalTickets,
+            msg.sender
+        );
         events.push(newEvent);
-        emit EventCreated(events.length - 1, _name, _date, _time, _price, _totalTickets);
+        emit EventCreated(
+            events.length - 1,
+            _name,
+            _date,
+            _time,
+            _price,
+            _totalTickets
+        );
     }
 
     // Function to buy tickets for an event
     function buyTicket(uint256 _eventId, uint256 _tickets) public payable {
-        require(msg.value >= events[_eventId].price * _tickets, "Not enough Ether provided.");
-        require(events[_eventId].totalTickets >= _tickets, "Not enough tickets available.");
+        require(
+            msg.value >= events[_eventId].price * _tickets,
+            "Not enough Ether provided."
+        );
+        require(
+            events[_eventId].totalTickets >= _tickets,
+            "Not enough tickets available."
+        );
         ticketsSold[_eventId][msg.sender] += _tickets;
         events[_eventId].totalTickets -= _tickets;
     }
@@ -48,8 +84,8 @@ contract EventManagement {
     }
 
     // Function to get the total number of events
-    function getTotalEvents() public view returns (uint256) {
-        return events.length;
+    function getTotalTickets(uint256 _eventId) public view returns (uint256) {
+        return events[_eventId].totalTickets;
     }
 
     // Function to get the total number of tickets for a specific event
