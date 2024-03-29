06de181d2a549b94c35ef22831f431105b00e45d
Icarus-afk <ahmed.ehsan1258@gmail.com>
2024-03-29 19:10:50 +0600

    logger added for the requests

--- server/app.js
+++ server/app.js
@@ -10,12 +10,14 @@ const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
 app.use(cors());
+app.use(errorHandler);
+app.use(logger);
 
 app.use('/user', userRoutes);
 app.use('/event', eventRoutes);
 
-app.use(errorHandler);
-app.use(logger);
+
 
 export default app;
--- server/middleware/logger.js
+++ server/middleware/logger.js
@@ -1,6 +1,29 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const logger = (req, res, next) => {
-    console.log(`Received ${req.method} request to ${req.originalUrl}`);
+    res.on('finish', () => {
+        const log = `Time: ${new Date().toLocaleString()}, Method: ${req.method}, URL: ${req.originalUrl}, Status: ${res.statusCode}\n`;
+        const logDir = path.join(__dirname, '../logs');
+        const logFile = path.join(logDir, 'request.log');
+
+        if (!fs.existsSync(logDir)) {
+            fs.mkdirSync(logDir);
+        }
+
+        fs.appendFile(logFile, log, err => {
+            if (err) {
+                console.error('Failed to write to log file:', err);
+            }
+        });
+    });
+
     next();
-  };
-  
-  export default logger;
+};
+
+export default logger;
