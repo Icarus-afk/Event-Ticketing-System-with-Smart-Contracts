1c5e4827b8fe18ea45e8c2d8b9fd4d27b3d095ba
Icarus-afk <ahmed.ehsan1258@gmail.com>
2024-04-03 21:57:57 +0600

    transfer ticket + Smart contract integration

--- server/controllers/ticket.js
+++ server/controllers/ticket.js
@@ -27,7 +27,7 @@ export const issueTicket = async (req, res) => {
         if (!req.userId) {
             return res.status(401).json({ success: false, message: 'User not authenticated', statusCode: 401 });
         }
-        
+
         const wallet = await Wallet.findOne({ userId: req.userId });
         if (!wallet) {
             return res.status(404).json({ success: false, message: 'User wallet not found', statusCode: 404 });
@@ -37,26 +37,24 @@ export const issueTicket = async (req, res) => {
         if (!from) {
             return res.status(400).json({ success: false, message: 'Wallet address not found', statusCode: 400 });
         }
-        
+
         const event = await Event.findOne({ eventId: eventId });
         if (!event) {
             return res.status(404).json({ success: false, message: 'Event not found', statusCode: 404 });
         }
 
-        console.log(event)
-        
         const totalTickets = event.totalTickets;
         const ticketsSold = await Ticket.countDocuments({ eventId: event._id });
 
         if (ticketsSold >= totalTickets) {
             return res.status(400).json({ success: false, message: 'No more tickets available for this event', statusCode: 400 });
         }
-        
+
         const hasTicket = await TicketContract.methods.getTicketDetails(from, eventId).call();
         if (hasTicket) {
             return res.status(400).json({ success: false, message: 'Ticket already issued to this address for the event', statusCode: 400 });
         }
-        
+
         const decipher = crypto.createDecipheriv('aes-256-ctr', ENCRYPTION_KEY, Buffer.from(wallet.iv, 'hex'));
         let decryptedPrivateKey = decipher.update(wallet.privateKey, 'hex', 'utf8');
         decryptedPrivateKey += decipher.final('utf8');
@@ -68,20 +66,20 @@ export const issueTicket = async (req, res) => {
             'to': contractAddress,
             'gasPrice': web3Instance.utils.toHex(20 * 1e9), // 20 Gwei
             'gasLimit': web3Instance.utils.toHex(210000), // gas limit
-            'data': TicketContract.methods.issueTicket(from, eventId).encodeABI() // calling the issueTicket function in the smart contract
+            'data': TicketContract.methods.issueTicket(from, eventId).encodeABI()
         };
 
         const signedTx = await web3Instance.eth.accounts.signTransaction(tx, decryptedPrivateKey);
         const receipt = await web3Instance.eth.sendSignedTransaction(signedTx.rawTransaction);
         console.log(`Transaction hash: ${receipt.transactionHash}`);
-        
+
         const ticket = new Ticket({
             eventId: event._id,
             owner: req.userId,
             isTransferred: false
         });
         await ticket.save();
-        
+
         return res.status(200).json({ success: true, message: 'Ticket issued successfully', statusCode: 200 });
 
     } catch (error) {
@@ -95,13 +93,11 @@ export const getTotalTicketsSold = async (req, res) => {
         console.log('Getting total tickets...');
         const eventId = req.params.eventId;
 
-        // Find the event using the eventId
         const event = await Event.findOne({ eventId: eventId });
         if (!event) {
             return res.status(404).json({ success: false, message: 'Event not found', statusCode: 404 });
         }
 
-        // Use the ObjectId of the event to count the tickets
         const totalTickets = await Ticket.countDocuments({ eventId: event._id });
 
         return res.status(200).json({ success: true, totalTickets, statusCode: 200 });
@@ -129,28 +125,67 @@ export const getTicketsTransfarred = async (req, res) => {
 export const transferTicket = async (req, res) => {
     try {
         console.log('Transferring ticket...');
-        const { to } = req.body;
+        const { to, ticketId } = req.body;  // Extract ticketId from the request body
 
-        const from = req.userId;
+        const fromWallet = await Wallet.findOne({ userId: req.userId });
+        if (!fromWallet) {
+            return res.status(404).json({ success: false, message: 'Sender wallet not found', statusCode: 404 });
+        }
 
-        const ticket = await Ticket.findOne({ owner: from });
+        const recipientUser = await User.findOne({ email: to });
+        if (!recipientUser) {
+            return res.status(404).json({ success: false, message: 'Recipient user not found', statusCode: 404 });
+        }
+
+        const toWallet = await Wallet.findOne({ userId: recipientUser._id });
+        if (!toWallet) {
+            return res.status(404).json({ success: false, message: 'Recipient wallet not found', statusCode: 404 });
+        }
+
+        const ticket = await Ticket.findById(ticketId);
         if (!ticket) {
             return res.status(404).json({ success: false, message: 'Ticket not found', statusCode: 404 });
         }
 
-        const recipient = await User.findOne({ email: to });
-        if (!recipient) {
-            return res.status(404).json({ success: false, message: 'Recipient user not found', statusCode: 404 });
+
+        const eventObjectId = ticket.eventId;
+        const event = await Event.findById(eventObjectId);
+
+        if (!event) {
+            return res.status(404).json({ success: false, message: 'Event not found', statusCode: 404 });
         }
 
-        ticket.owner = recipient._id;
-        ticket.isTransferred = true;
+        const eventId = event.eventId;
+
+        const decipher = crypto.createDecipheriv('aes-256-ctr', ENCRYPTION_KEY, Buffer.from(fromWallet.iv, 'hex'));
+        let decryptedPrivateKey = decipher.update(fromWallet.privateKey, 'hex', 'utf8');
+        decryptedPrivateKey += decipher.final('utf8');
+
+        const nonce = await web3Instance.eth.getTransactionCount(fromWallet.address);
+
+        const tx = {
+            'nonce': nonce,
+            'to': contractAddress,
+            'gasPrice': web3Instance.utils.toHex(20 * 1e9),
+            'gasLimit': web3Instance.utils.toHex(210000),
+            'data': TicketContract.methods.transferTicket(toWallet.address, eventId).encodeABI()
+        };
+        const signedTx = await web3Instance.eth.accounts.signTransaction(tx, decryptedPrivateKey);
+        const receipt = await web3Instance.eth.sendSignedTransaction(signedTx.rawTransaction);
+        console.log(`Transaction hash: ${receipt.transactionHash}`);
+ 
+        const newOwner = await User.findOne({ email: req.body.to });
+        if (!newOwner) {
+            return res.status(404).json({ success: false, message: 'New owner not found', statusCode: 404 });
+        }
+
+        ticket.owner = newOwner._id;
         await ticket.save();
 
         return res.status(200).json({ success: true, message: 'Ticket transferred successfully', ticket, statusCode: 200 });
     } catch (error) {
         console.error(error);
-        return res.status(500).json({ success: false, message: 'Server error', statusCode: 500 });
+        return res.status(500).json({ success: false, message: 'Something went wrong, Contact the Admin for further details', statusCode: 500 });
     }
 };
 
@@ -169,7 +204,7 @@ export const getTicketDetails = async (req, res) => {
         }
 
         const ticket = await Ticket.findOne({ owner: ownerId, eventId: event._id })
-            .populate('owner', '-password') 
+            .populate('owner', '-password')
             .populate('eventId');
 
         const hasTicket = !!ticket;
