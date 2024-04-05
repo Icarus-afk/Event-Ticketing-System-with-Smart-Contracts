44f9ad233440d654cf0251f4a40faa693e291e87
Icarus-afk <ahmed.ehsan1258@gmail.com>
2024-04-05 15:18:53 +0600

    client code remove (for now)

--- server/app.js
+++ server/app.js
@@ -13,7 +13,11 @@ const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }));
 app.use(errorHandler);
 app.use(logger);
 app.use(limiter);
